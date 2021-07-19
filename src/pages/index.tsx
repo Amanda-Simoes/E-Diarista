9977019798ac7489eaa15ca153830ae0beacdfc8
Amanda <amanda.simoes1707@gmail.com>
2021-07-19 14:38:18 -0300

    Primeiro componente

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '@styles/Home.module.css'
+import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironment'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      
+    <div>
+      <SafeEnvironment/>
     </div>
   )
 }
